6ca96142dace2877f3bcd8a6f561cdaa32c355e5
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-02 15:52:37 +0530

    Fix typo in title attribute for index method

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Graddle");
+        model.addAttribute("title", "I have successfuly built a sprint boot application using Graddle+Maven");
         model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
         return "index";
     }
